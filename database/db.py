7eff53ced864d7b163990602b7f93404aee89fb0
gaojian15 <gaojian15@baidu.com>
2023-07-03 10:18:02 +0800

    add conn pool

--- database/db.py
+++ database/db.py
@@ -14,7 +14,7 @@ def init_mysql_by_conf(conf_file:str) -> sqlalchemy.engine.Engine:
     """
     cfg = conf_parse.load_toml_config(conf_file)
     dsn = 'mysql+pymysql://{User}:{Password}@{Host}:{Port}/{DBName}?charset={Charset}&use_unicode=1'.format_map(cfg)
-    engine = sqlalchemy.create_engine(dsn)
+    engine = sqlalchemy.create_engine(dsn, pool_size=cfg['PoolSize'], pool_recycle=cfg['PoolRecycle'])
     return engine
 
 if __name__ =='__main__':
